191593dab6c9b2fe1410dc7ae5eb8ba906aba42c
YubinYuan <45218572+YubinYuan@users.noreply.github.com>
2024-11-21 21:36:52 +0800

    Update train.sh

--- exp/train.sh
+++ exp/train.sh
@@ -5,7 +5,7 @@ python3 train.py --task mot \
                  --load_model '../models/tlsh_mot.pth'\
                  --data_cfg '../src/lib/cfg/visdrone.json'\
                  --gpus '0,1,2,3'\
-                 --lr_step '20'\
-                 --lr 7e-5 \
-                 --num_epochs 30
+                 --lr_step '40'\
+                 --lr 9e-5 \
+                 --num_epochs 70
 cd ..
